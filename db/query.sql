6d76b71abee16058c5d2d22992650c3b250099dd
Tanya Silyutina <48433669+TanyaSilyutina@users.noreply.github.com>
2023-05-03 22:59:52 -0700

    added view employees functionality in index.js

--- db/query.sql
+++ db/query.sql
@@ -1,16 +1,25 @@
 USE tracker_db;
 
+SELECT *
+FROM employee;
 
-# SELECT * FROM employee;
+# SELECT role.id, role.title, department.department, role.salary
+# FROM role, department
+# WHERE role.department_id = department.id
+# ORDER BY role.id
 
-SELECT role.id, role.title, department.department, role.salary
-FROM role, department
-WHERE role.department_id = department.id
-ORDER BY role.id
-#
-# -- INSERT INTO movies (movie_name) VALUES ("Memento");
-#
-# UPDATE reviews SET review = "BAAAAAAAAAD MOVIE" WHERE movie_id = 1;
-#
-# SELECT * FROM reviews;
-
+SELECT employee.id,
+       employee.first_name,
+       employee.last_name,
+       role.title,
+       department.department,
+       role.salary,
+       (SELECT CONCAT(first_name, " ", last_name)
+        FROM employee AS manager
+        where manager.id = employee.manager_id) as manager
+FROM employee,
+     role,
+     department
+WHERE employee.role_id = role.id
+  AND role.department_id = department.id
+ORDER BY role.id;
--- index.js
+++ index.js
@@ -19,8 +19,32 @@ async function init() {
         if(choice === "Quit") {
             break;
         } else if (choice === "View all employees") {
-
+            let query = await db.query(
+                'SELECT employee.id,\n' +
+                '       employee.first_name,\n' +
+                '       employee.last_name,\n' +
+                '       role.title,\n' +
+                '       department.department,\n' +
+                '       role.salary,\n' +
+                '       (SELECT CONCAT(first_name, " ", last_name)\n' +
+                '        FROM employee AS manager\n' +
+                '        where manager.id = employee.manager_id) as manager\n' +
+                'FROM employee,\n' +
+                '     role,\n' +
+                '     department\n' +
+                'WHERE employee.role_id = role.id\n' +
+                '  AND role.department_id = department.id\n' +
+                'ORDER BY role.id;'
+            );
+            console.log(cTable.getTable(query[0]));
         } else if (choice === "View all roles") {
+            let query = await db.query(
+                'SELECT role.id, role.title, department.department, role.salary\n' +
+                'FROM role, department\n' +
+                'WHERE role.department_id = department.id\n' +
+                'ORDER BY role.id'
+            );
+            console.log(cTable.getTable(query[0]));
 
         } else if (choice === "View all departments") {
             let query = await db.query('SELECT * FROM department');
